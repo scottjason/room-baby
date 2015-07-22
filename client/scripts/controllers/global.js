efeb0856bdc3a5fd28e909d69715db2de08e3165
Scott Jason <scottleejason@gmail.com>
2015-07-22 04:41:24 -0700

    sets max width for mobile

--- client/scripts/controllers/global.js
+++ client/scripts/controllers/global.js
@@ -3,12 +3,16 @@
 angular.module('RoomBaby')
   .controller('GlobalCtrl', GlobalCtrl);
 
-function GlobalCtrl($scope, PubSub) {
+function GlobalCtrl($scope, PubSub, DeviceService) {
+
+  $scope.isMobile = function() {
+    return DeviceService.isMobile();
+  };
 
   $scope.registerEvents = function() {
     PubSub.on('toggleOverflow', function(_bool) {
       $scope.showOverflow = _bool;
     });
   };
-  GlobalCtrl.$inject['$scope', 'PubSub'];
+  GlobalCtrl.$inject['$scope', 'PubSub', 'DeviceService'];
 }
