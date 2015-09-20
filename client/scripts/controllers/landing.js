4112c8fe0ec6a9dd128c7182340dd9c414d7f29f
Scott Jason <scottleejason@gmail.com>
2015-09-19 22:17:41 -0700

    completes registration ui refactor

--- client/scripts/controllers/landing.js
+++ client/scripts/controllers/landing.js
@@ -144,6 +144,7 @@ function LandingCtrl($scope, $rootScope, $state, $window, $timeout, Validator, S
   };
 
   this.validateRegistration = function() {
+    console.log('reg called');
     var inProgress = StateService.data['Auth'].Registration.inProgress;
     if (!inProgress) {
       StateService.data['Auth'].Registration.inProgress = true;
